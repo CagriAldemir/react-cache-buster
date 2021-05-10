ef4e6baa754aa0b2383c1c76510a78bab265b268
Çağrı Aldemir <cagri.aldemir@hotmail.com>
2021-05-10 09:38:02 +0300

    Initial commit

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-cache-buster'
+import 'react-cache-buster/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
