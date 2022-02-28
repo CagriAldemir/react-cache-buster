782e464971799f2741692de14ec0da1c49983d2f
Gregory Christie <greychrist@users.noreply.github.com>
2022-02-27 20:39:16 -0500

    Adding some minor error handling

--- src/generate-meta-tag.js
+++ src/generate-meta-tag.js
@@ -4,24 +4,31 @@ const path = require('path');
 const packageJson = require(`${process.cwd()}/package.json`);
 const metaJson = path.join(process.cwd(), 'public', 'meta.json');
 
-const appVersion = packageJson.version;
-const revision = require('child_process')
-  .execSync('git rev-parse HEAD')
-  .toString()
-  .trim();
+let appVersion = 'no-version-found';
+let gitHash = 'no-git-hash-found';
+try{
+  appVersion = packageJson.version;
+  gitHash = require('child_process')
+    .execSync('git rev-parse HEAD')
+    .toString()
+    .trim();
+}
+catch(ex){ }
 
 const jsonData = {
   version: appVersion, 
-  hash: revision
+  hash: gitHash
 };
 
 const jsonContent = JSON.stringify(jsonData);
 
 writeFile(metaJson, jsonContent, 'utf8', function (err) {
   if (err) {
-    console.error('An error occurred while writing JSON Object to meta.json');
+    console.error(
+      'CacheBuster: An error occurred while writing JSON Object to meta.json'
+    );
     throw console.error(err);
   } else {
-    console.log(`meta.json file has been saved with v${appVersion}`);
+    console.log(`CacheBuster: meta.json file has been saved with v${appVersion} and gitHash: ${gitHash}`);
   }
 });
